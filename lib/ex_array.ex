5d0ce9cd6ca8233a8e3d87c8a17bfb27085bb7c0
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-04-20 22:19:24 -0600

    Commit initial

---
+++ lib/ex_array.ex
@@ -0,0 +1,18 @@
+defmodule ExArray do
+  @moduledoc """
+  Documentation for `ExArray`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExArray.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_array_test.exs
@@ -0,0 +1,8 @@
+defmodule ExArrayTest do
+  use ExUnit.Case
+  doctest ExArray
+
+  test "greets the world" do
+    assert ExArray.hello() == :world
+  end
+end
