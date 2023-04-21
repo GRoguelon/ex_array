bafcdad78b3f485966f145cbb14a2813745119cc
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-04-21 12:26:22 -0600

    Refactored Enumerable.ExArray

--- lib/ex_array/enumerable.ex
+++ lib/ex_array/enumerable.ex
@@ -1,25 +1,25 @@
 defimpl Enumerable, for: ExArray do
   @moduledoc false
 
-  @spec count(ExArray.t()) :: {:ok, non_neg_integer()}
+  @spec count(@for.t()) :: {:ok, non_neg_integer()}
   def count(arr) do
-    {:ok, ExArray.size(arr)}
+    {:ok, @for.size(arr)}
   end
 
-  @spec member?(ExArray.t(), ExArray.element()) :: {:error, __MODULE__}
+  @spec member?(@for.t(), @for.value()) :: {:error, __MODULE__}
   def member?(_arr, _value) do
     {:error, __MODULE__}
   end
 
-  @spec reduce(ExArray.t(), Enumerable.acc(), Enumerable.reducer()) :: Enumerable.result()
-  def reduce(%ExArray{content: c}, acc, fun) do
-    Enumerable.reduce(:array.to_list(c), acc, fun)
+  @spec reduce(@for.t(), Enumerable.acc(), Enumerable.reducer()) :: Enumerable.result()
+  def reduce(arr, acc, fun) do
+    Enumerable.reduce(@for.to_list(arr), acc, fun)
   end
 
-  @spec slice(ExArray.t()) :: {:ok, size :: non_neg_integer(), Enumerable.to_list_fun()}
-  def slice(%ExArray{content: c}) do
-    size = :array.size(c)
+  @spec slice(@for.t()) :: {:ok, size :: non_neg_integer(), Enumerable.to_list_fun()}
+  def slice(arr) do
+    size = @for.size(arr)
 
-    {:ok, size, &ExArray.to_list/1}
+    {:ok, size, &@for.to_list/1}
   end
 end
