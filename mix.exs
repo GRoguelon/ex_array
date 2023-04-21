6ddf013f8a4a4eb31335ad56309c9979da20a739
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-04-21 16:14:28 -0600

    Added mix_test_watch

--- mix.exs
+++ mix.exs
@@ -13,7 +13,10 @@ defmodule ExArray.MixProject do
       deps: deps(),
       docs: docs(),
       description: "A wrapper module for Erlang's array.",
-      source_url: "https://github.com/groguelon/ex_array"
+      source_url: "https://github.com/groguelon/ex_array",
+      preferred_cli_env: [
+        "test.watch": :test
+      ]
     ]
   end
 
@@ -53,7 +56,8 @@ defmodule ExArray.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
